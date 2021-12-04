6e8d1d261951f563fc10c9a5d548bcbe03a4c6cc
Saiban Hussein <shhussein@m.marywood.edu>
2021-12-03 18:02:36 -0600

    Updates

---
+++ precache-manifest.a47f2f7d6d288240572e09cd99cdd058.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d269e17aebbba596ef32176fbc7ab0c6",
+    "url": "/test3/index.html"
+  },
+  {
+    "revision": "bd1072f19b48c77d8cfd",
+    "url": "/test3/static/css/main.ad94a53e.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/test3/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/test3/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604a1622df287d1932aa",
+    "url": "/test3/static/js/3.1d36f085.chunk.js"
+  },
+  {
+    "revision": "bd1072f19b48c77d8cfd",
+    "url": "/test3/static/js/main.401352c4.chunk.js"
+  },
+  {
+    "revision": "8c03a703a7ad267c733a",
+    "url": "/test3/static/js/runtime-main.9814ca1e.js"
+  },
+  {
+    "revision": "e4a20cf406ff0a31378d7ab33ba8c1ba",
+    "url": "/test3/static/media/Owais' Resume.e4a20cf4.pdf"
+  },
+  {
+    "revision": "5035699ad38fd5313bc95c4891f53581",
+    "url": "/test3/static/media/auto-tracker-image.5035699a.png"
+  },
+  {
+    "revision": "09e6ae3da5daad49c278e9ff4a75743d",
+    "url": "/test3/static/media/budget-tracker-image.09e6ae3d.png"
+  },
+  {
+    "revision": "d92efe2500ba8695d4ae6432f75b5cf9",
+    "url": "/test3/static/media/flash-beats-image.d92efe25.gif"
+  },
+  {
+    "revision": "d8671f834c3c32005de3dcb406167c84",
+    "url": "/test3/static/media/living-real-image.d8671f83.png"
+  },
+  {
+    "revision": "fd14fff8beb9ae1bb3fcdf093419fe49",
+    "url": "/test3/static/media/note-taker-image.fd14fff8.png"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/test3/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "e232e25f978fc2e6a3f3960c7dbcb124",
+    "url": "/test3/static/media/recent-photo.e232e25f.jpg"
+  },
+  {
+    "revision": "5db07b5ca4b360c2f9058145e3070ff4",
+    "url": "/test3/static/media/weather-dashboard-image.5db07b5c.png"
+  },
+  {
+    "revision": "2524a8b5b4006da1d062430b9f8b0c05",
+    "url": "/test3/static/media/webpage-screenshot.2524a8b5.png"
+  }
+]);
